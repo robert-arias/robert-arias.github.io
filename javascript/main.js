5009fe0e777f1e0e2e107aad528fd87b26bd292e
robert-arias <robertariascastro@outlook.com>
2020-07-10 22:59:00 -0600

    js minor changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,60 +1,55 @@
 $('.burger').click(() => {
-    $('.burger__line').toggleClass("burger__line--open");
-    $(".nav__list").toggleClass("nav__list--open");
+    $('.burger__line').toggleClass('burger__line--open');
+    $('.nav__list').toggleClass('nav__list--open');
 });
 
 $('.nav__link').click(() => {
-  $('.burger__line').toggleClass("burger__line--open");
-  $(".nav__list").toggleClass("nav__list--open");
+  $('.burger__line').toggleClass('burger__line--open');
+  $('.nav__list').toggleClass('nav__list--open');
 })
 
 //from: https://codepen.io/Danielgroen/pen/VeRPOq
-document.addEventListener('DOMContentLoaded',function(event){
-    // array with texts to type in typewriter
-    var dataText = ["Hi, <br> I'm Robert Arias."];
-    
-    // type one text in the typwriter
-    // keeps calling itself until the text is finished
-    function typeWriter(text, i, fnCallback) {
-      // chekc if text isn't finished yet
-      
-
-      if (i < (text.length)) {
-
-        if ((text.substring(i, i+1).localeCompare("<")) != 0) {
-            // add next character to h1
-            document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span class="ticking" aria-hidden="true"></span>';
-            // wait for a while and call this function again for next character
-        }
-
-        setTimeout(function() {
-          typeWriter(text, i + 1, fnCallback)
-        }, 100);
+$(document).on('DOMContentLoaded', () => {
+  // array with texts to type in typewriter
+  const dataText = ["Hi, <br> I'm Robert Arias."];
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  const typeWriter = (text, i, fnCallback) => {
+    // check if text isn't finished yet
+    if (i < (text.length)) {
+      if ((text.substring(i, i+1).localeCompare("<")) != 0) {
+          // add next character to h1
+          $('.landing-page__title').html(text.substring(0, i+1) +'<span class="ticking" aria-hidden="true"></span>');
+          // wait for a while and call this function again for next character
       }
-      // text finished, call callback if there is a callback function
-      else if (typeof fnCallback == 'function') {
-        // call callback after timeout
-        setTimeout(fnCallback, 700);
-      }
+      setTimeout(() => {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
     }
-    // start a typewriter animation for a text in the dataText array
-     function StartTextAnimation(i) {
-       if (typeof dataText[i] == 'undefined'){
-          setTimeout(function() {
-            StartTextAnimation(0);
-          }, 20000);
-       }
-       // check if dataText[i] exists
-      if (i < dataText[i].length) {
-        // text exists! start typewriter animation
-       typeWriter(dataText[i], 0, function(){
-         // after callback (and whole text has been animated), start next text
-         StartTextAnimation(i + 1);
-       });
-      }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
     }
-    // start the text animation
-    StartTextAnimation(0);
+  }
+  // start a typewriter animation for a text in the dataText array
+   const StartTextAnimation = (i) => {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(() => {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
 });
 
 //from https://css-tricks.com/snippets/jquery/smooth-scrolling/
