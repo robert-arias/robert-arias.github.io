66c4fc045d7040c5ceebc9a9d3b484bd22457af8
robert-arias <robertariascastro@outlook.com>
2020-07-10 23:01:34 -0600

    js minor changes

--- javascript/main.js
+++ javascript/main.js
@@ -70,18 +70,7 @@ $('a[href*="#"]').click(function(event) {
       event.preventDefault();
       $('html, body').animate({
         scrollTop: target.offset().top
-      }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
+      }, 1000);
     }
   }
 });
