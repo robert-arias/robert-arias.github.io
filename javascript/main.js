bcff7f4f8f46505499d03f26707db221e90e8522
robert-arias <robertariascastro@outlook.com>
2020-07-10 22:32:56 -0600

    smooth scrolling

--- javascript/main.js
+++ javascript/main.js
@@ -55,4 +55,38 @@ document.addEventListener('DOMContentLoaded',function(event){
     }
     // start the text animation
     StartTextAnimation(0);
-  });
+});
+
+//from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+// Select all links with hashes
+$('a[href*="#"]').click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
