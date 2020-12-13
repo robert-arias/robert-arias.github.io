192d229d4e1615514794a56c40b9dda0da0e8ff1
robert-arias <robertariascastro@outlook.com>
2020-12-12 20:46:21 -0600

    finished main button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { MainButton } from './components/buttons/buttons';
+import ArrowSVG from "./assets/arrow.svg";
 
 function App() {
   return (
     <div className="App">
-      <MainButton link_to="#" title="button" />
+      <MainButton link_to="#" title="Hire me" />
+      <br />
+      <br />
+      <MainButton link_to="#" title="See more" img={ArrowSVG}/>
+      <br />
+      <br />
     </div>
   );
 }
--- src/components/buttons/buttons.js
+++ src/components/buttons/buttons.js
@@ -1,5 +1,12 @@
-export function MainButton({ link_to, title }) {
-    return (
-        <a href={link_to} className="btn btn--main" title={title}>{title}</a>
-    );
+export function MainButton({ link_to, title, img }) {
+  return (
+    <a
+      href={link_to}
+      className={`btn ${img ? `btn--main-img` : `btn--main`}`}
+      title={title}
+    >
+      <span>{title}</span>
+      {img ? <img className="btn__img" src={img} alt="" /> : null}
+    </a>
+  );
 }
